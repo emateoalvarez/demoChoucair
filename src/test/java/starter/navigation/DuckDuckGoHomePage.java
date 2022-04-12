404c06ce5d8d0de120bcb0de3241926b5a1a73ac
emateoalvarez <39784540+emateoalvarez@users.noreply.github.com>
2022-04-11 23:17:32 -0500

    set template packages

---
+++ src/test/java/starter/navigation/DuckDuckGoHomePage.java
@@ -0,0 +1,8 @@
+package starter.navigation;
+
+import net.serenitybdd.core.pages.PageObject;
+import net.thucydides.core.annotations.DefaultUrl;
+
+@DefaultUrl("https://duckduckgo.com")//) https://serenity.is/demo/
+public class DuckDuckGoHomePage extends PageObject {//serenityHomePage
+}
